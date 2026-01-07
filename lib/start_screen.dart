dba1e53a479bf6df064fd4178032cae53fb9c3d0
Arthur Lima <arthurlima@192.168.0.15>
2026-01-07 19:43:55 -0300

    main: Criando start page

---
+++ lib/start_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  void startQuiz() {
+  }
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Image(
+            image: AssetImage("assets/images/quiz-logo.png"),
+            height: 300,
+            color: Color.fromARGB(119, 255, 255, 255),
+          ),
+          const SizedBox(height: 60),
+          const Text(
+            "Learn Flutter the fun way!",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20
+            ),
+          ),
+          const SizedBox(height: 20),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            icon: const Icon(
+              Icons.arrow_forward,
+              color: Colors.white,  
+            ),
+            label: const Text(
+              "Start Quiz",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            )
+          )
+        ]
+      )
+    );
+  }
+}
